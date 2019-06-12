68ca38956223de9991a8c9b9bec0ca7b217770f6
Yiwen Xue <15225434259xue@gmail.com>
2019-06-12 12:40:49 +0300

    Just to make backup

---
+++ build/CMakeFiles/mylib.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/arthas/program/mice_physiology/src/file.c" "/home/arthas/program/mice_physiology/build/CMakeFiles/mylib.dir/src/file.c.o"
+  "/home/arthas/program/mice_physiology/src/mathematic.c" "/home/arthas/program/mice_physiology/build/CMakeFiles/mylib.dir/src/mathematic.c.o"
+  "/home/arthas/program/mice_physiology/src/plot.c" "/home/arthas/program/mice_physiology/build/CMakeFiles/mylib.dir/src/plot.c.o"
+  "/home/arthas/program/mice_physiology/src/utility.c" "/home/arthas/program/mice_physiology/build/CMakeFiles/mylib.dir/src/utility.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
